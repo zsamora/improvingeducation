d4d339ea6df2c91183eef21ed73b5138ad709a27
zsamora <zsamora@ug.uchile.cl>
2017-07-02 18:10:05 -0400

    test

--- enviar.php
+++ enviar.php
@@ -5,11 +5,9 @@ $ids = $_POST['id_result'];
 $selecciones = $_POST['seleccion_result'];
 $proceso = $_SESSION['proceso_id'];
 $i = 0;
-for ($i = 0 ; $i < count($ids) ; $i++){
+/*for ($i = 0 ; $i < count($ids) ; $i++){
       $eval_id = $ids[$i];
       $sel_id = $selecciones[$i];
-      $insertar_sql ="INSERT INTO resultados_ind (evaluacion_id,respuesta) VALUES ($eval_id,$sel_id)";
-      $insertar_result = $conn->query($insertar_sql);// or die ("database error:". $conn->error);
       $delete_sql = "DELETE FROM resultados_ind WHERE evaluacion_id = $eval_id";
       $delete_result = $conn->query($delete_sql);// or die ("database error:". $conn->error);
       $insertar_sql ="INSERT INTO resultados_ind (evaluacion_id,respuesta) VALUES ($eval_id,$sel_id)";
@@ -22,6 +20,6 @@ for ($i = 0 ; $i < count($ids) ; $i++){
         //$update_sql = "UPDATE resultados_ind SET respuesta = $sel_id WHERE evaluacion_id = $eval_id";
         //$update_result = $conn->query($update_sql) or die ("database error:". $conn->error);
       //}
-}
-echo 1;
+//}
+echo $proceso;
 ?>
