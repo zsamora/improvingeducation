4d6d4547e8b55e83fe8493e94139600949e3aa5c
zsamora <zsamora@ug.uchile.cl>
2017-07-02 18:07:45 -0400

    test

--- enviar.php
+++ enviar.php
@@ -9,11 +9,11 @@ for ($i = 0 ; $i < count($ids) ; $i++){
       $eval_id = $ids[$i];
       $sel_id = $selecciones[$i];
       $insertar_sql ="INSERT INTO resultados_ind (evaluacion_id,respuesta) VALUES ($eval_id,$sel_id)";
-      $insertar_result = $conn->query($insertar_sql) or die ("database error:". $conn->error);
+      $insertar_result = $conn->query($insertar_sql);// or die ("database error:". $conn->error);
       $delete_sql = "DELETE FROM resultados_ind WHERE evaluacion_id = $eval_id";
-      $delete_result = $conn->query($delete_sql) or die ("database error:". $conn->error);
+      $delete_result = $conn->query($delete_sql);// or die ("database error:". $conn->error);
       $insertar_sql ="INSERT INTO resultados_ind (evaluacion_id,respuesta) VALUES ($eval_id,$sel_id)";
-      $insertar_result = $conn->query($insertar_sql) or die ("database error:". $conn->error);
+      $insertar_result = $conn->query($insertar_sql);// or die ("database error:". $conn->error);
       /*$existe_sql = "SELECT * FROM resultados_ind WHERE evaluacion_id = $eval_id";
       $existe_result = $conn->query($existe_sql) or die("database error:". $conn->error);
       if ($existe_result->num_rows == 0){
