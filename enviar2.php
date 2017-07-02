3be3e9071f5e34ceac252b1851236f0ef249fa68
zsamora <zsamora@ug.uchile.cl>
2017-07-02 15:24:25 -0400

    test

--- enviar2.php
+++ enviar2.php
@@ -3,7 +3,7 @@ session_start();
 include_once("db_connect.php");
 $ids = $_POST['id_result'];
 $selecciones = $_POST['seleccion_result'];
-// TODO: Cambiar por $i in $ids
+$i = 0;
 for ($i = 0 ; $i < count($ids) ; $i++){
       $eval_id = $ids[$i];
       $sel_id = $selecciones[$i];
