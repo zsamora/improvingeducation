9597a39efd448191fbe89c5c268ba07e95d6a088
zsamora <zsamora@ug.uchile.cl>
2017-07-02 15:26:41 -0400

    test

--- enviar2.php
+++ enviar2.php
@@ -8,7 +8,7 @@ for ($i = 0 ; $i < count($ids) ; $i++){
       $eval_id = $ids[$i];
       $sel_id = $selecciones[$i];
       $existe_sql = "SELECT * FROM resultados_comp WHERE evaluacion_id = $eval_id";
-      $existe_result = $conn->query($existe_sql) or die("database error:". $conn->error);
+      $existe_result = $conn->query($existe_sql);
       if ($existe_result->num_rows == 0){
         $insertar_sql ="INSERT INTO resultados_comp (evaluacion_id,respuesta) VALUES ($eval_id,$sel_id)";
         $insertar_result = $conn->query($insertar_sql) or die ("database error:". $conn->error);
